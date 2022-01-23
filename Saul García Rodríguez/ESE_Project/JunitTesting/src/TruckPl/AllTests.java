7e6d2f5f3b1c8eb7d3ffde1a52e864d23265dbf2
Saul García Rodríguez <93392906+SaulGarciaR@users.noreply.github.com>
2022-01-23 23:59:57 +0100

    Add files via upload

---
+++ Saul García Rodríguez/ESE_Project/JunitTesting/src/TruckPl/AllTests.java
@@ -0,0 +1,11 @@
+package TruckPl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PedestrianTest.class, VehicleTest.class, GapTest.class })
+public class AllTests {
+
+}
